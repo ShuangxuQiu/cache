745ba1689f384029b57281e1299c7a1921c8277e
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2015-12-01 23:43:28 -0200

    Write server log and interrupt handler
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

--- server.py
+++ server.py
@@ -1,10 +1,34 @@
 import config
 import socket
 import time
+import signal
+import sys
+
+# Abre o arquivo de log do servidor
+log = open(config.LOG_SERVER,'a')
+
+# Gerenciador de interrupcoes
+def interrupt_handler(signal, frame):
+    print('\n')
+    server_print('Finalizacao', 'Servidor encerrado com sucesso!')
+    sys.exit(0)
 
 # Imprime uma mensagem no formato do servidor
 def server_print(stepname, message):
-    print('[' + time.ctime() + ' - ' + stepname + '] ' + message),
+    # Abre o arquivo de log do servidor
+    log = open(config.LOG_SERVER,'a')
+    # Se o tipo do passo for 'Mensagem', imprime apenas a mensagem	
+    if stepname == 'Mensagem':
+        # Imprime no log
+        log.write(message + '\n')
+	    # Imprime na tela
+        print(message)
+    else:
+        # Imprime no log
+        log.write('[' + time.ctime() + ' - ' + stepname + '] ' + message)
+	    # Imprime na tela
+        print('[' + time.ctime() + ' - ' + stepname + '] ' + message),
+
 
 # Define a tabela de dados como vazia
 table_data = { }
@@ -24,26 +48,31 @@ with open(config.SERVER_DATAFILE, 'r') as input_file:
             key, sep, value = data.partition('=>')
             table_data[key.strip()] = value.strip()
 
-print('Feito!')
-server_print('Inicializacao', 'Criando socket do servidor... ')
 
+server_print('Mensagem', 'Feito!')
+
+server_print('Inicializacao', 'Criando socket do servidor... ')
 # Cria o socket do servidor
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print('Feito!')
-server_print('Inicializacao', 'Vinculando servidor ao endereco e porta configurados... ')
+server_print('Mensagem', 'Feito!')
 
+server_print('Inicializacao', 'Vinculando servidor ao endereco e porta configurados... ')
 # Vincula o servidor ao host e porta
 s.bind((config.SERVER_HOST, config.SERVER_PORT))
 
-print('Feito!')
-server_print('Inicializacao', 'Colocando servidor em modo de escuta... ')
+server_print('Mensagem', 'Feito!')
 
+server_print('Inicializacao', 'Colocando servidor em modo de escuta... ')
 # Fica em modo escuta, aguardando conexoes de clientes
 s.listen(5)
 
-print('Feito!')
-server_print('Inicializacao', 'Servidor executando em ' + str(config.SERVER_HOST) + ':' + str(config.SERVER_PORT) + '\n')
+server_print('Mensagem', 'Feito!')
+
+server_print('Inicializacao', 'Servidor executando em ' + str(config.SERVER_HOST) + ':' + str(config.SERVER_PORT) + '\n\n')
+
+# Ativa o gerenciador de interrupcoes
+signal.signal(signal.SIGINT, interrupt_handler)
 
 while 1:
     # Aceita conexao e obtem socket e endereco do cliente
@@ -52,14 +81,21 @@ while 1:
 
     # Obtem a requisicao do cliente
     request = client_socket.recv(config.MAX_REQUEST_LENGTH).decode('utf8')
-    server_print('Requisicao', str(address[0]) + ':' + str(address[1]) + ' -> \"' + request + '\", processando e enviando resposta... ')
+    server_print('Requisicao', str(address[0]) + ':' + str(address[1]) + ' -> \"' + request + '\", processando...\n')
 
     # Envia a resposta ao cliente (informacao relativa a requisicao)
     if request in table_data:
+        # Imprime mensagem avisando o valor a ser enviado
+        server_print('Resposta', 'Enviando o valor \'' + table_data[request] + '\' para o cliente... ')
+        # Envia resposta ao cliente
         client_socket.send(table_data[request].encode('utf8'))
+        # Confirma o envio
+        server_print('Mensagem', 'Feito!\n')
     # Se nao houver nada definido para a requisicao, envia a string
     # definida no arquivo de configuracao
     else:
         client_socket.send(config.MESSAGE_REQUEST_NOT_FOUND.encode('utf8'))
+        # Imprime aviso de valor nao encontrado
+        server_print('Resposta', 'Valor nao encontrado para a requisicao!\n\n')
 
-    print('Feito!')
+
